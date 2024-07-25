b90ee53323a8e62cb53bf85118f5dec73853b00b
juliussiregar <juliussiregar1011@gmail.com>
2024-07-25 16:59:25 +0700

    fix image

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -9,12 +9,12 @@ const ProjectCard = ({ project }) => {
     <Card className='group overflow-hidden relative'>
       <CardHeader className='p-0'>
         {/* image */}
-        <div className='relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden'>
+        <div className='relative w-full h-[200px] sm:h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden'>
           <Image
             className='absolute bottom-0 shadow-2xl'
             src={project.image}
-            width={400}
-            height={400}
+            layout='fill'
+            objectFit='cover'
             alt=''
             priority
           />
--- components/Work.jsx
+++ components/Work.jsx
@@ -71,7 +71,7 @@ const Work = () => {
         {/* slider */}
         <div className='xl:max-w-[1000px] xl:absolute right-0 top-0'>
           <Swiper
-            className='h-[480px]'
+            className='h-[300px] sm:h-[400px] xl:h-[480px]'
             slidesPerView={1}
             breakpoints={{
               640: {
